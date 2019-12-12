02d1b95bcc99ff7ca3baf8c617985b98251d1783
Nikita <itomatpast@gmail.com>
2019-12-12 12:39:02 +0200

    completed 1 homework

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-let userBudget = prompt("Ваш бюджет?");
-let userCurrentDate = prompt("Введите дату в формате YYYY-MM-DD")
+let userBudget = prompt("Ваш бюджет на месяц");
+let userCurrentDate = prompt("Введите дату в формате YYYY-MM-DD");
+let userExpenses = prompt("Введите обязательную статью расходов в этом месяце");
+let userExpensesCost = prompt("Во сколько обойдется?");
 
 const appData = {
     budget: userBudget,
@@ -9,3 +11,11 @@ const appData = {
     income : [],
     savings: false
 }
+
+appData.expenses[userExpenses] = userExpensesCost
+
+let userBudgetOneDay = userBudget / 30;
+alert(userBudgetOneDay);
+
+
+
